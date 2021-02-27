bcdae172af5aa598b3b34c80be2a577ec8efab1c
Teymur <you@example.com>
2021-02-27 12:05:00 +0400

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+   let numberArray = n.toString().split('').map(Number);
+   let reverseArray = [];
+
+   for(let i = numberArray.length - 1; i >= 0; i = i - 1) {
+
+      reverseArray.push(numberArray[i]);
+
+   }
+
+   let reverseNum = reverseArray.join('');
+
+return Math.abs(parseInt(reverseNum) * Math.sign(n));
+
 }
